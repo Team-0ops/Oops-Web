d82db1257aa812f2d940067cd4d43a61da3b12c0
Junwoo <a90846251@gmail.com>
2025-11-02 18:31:49 +0900

    11/02 첫번째 섹션 글작성~글내용 입력 기본 스타일링 진행중

--- src/pages/postpage/PostWrite.tsx
+++ src/pages/postpage/PostWrite.tsx
@@ -1,11 +1,20 @@
 export const PostWrite = () => {
   return (
-    <div className="px-[82px] pt-[60px] rounded-[30px] bg-[#FDFDFD] flex flex-col justify-center">
+    <div className="px-[82px] pt-[60px] gap-20 rounded-[30px] bg-[#FDFDFD] flex flex-col justify-center">
       {/* 첫번째 섹션 글작성 ~ 글내용입력 */}
       <section className="flex flex-col gap-10">
-
+        <div>글 작성하기</div>
+        <input
+          className="border-[3px] border-solid placeholder:text-[#B3B3B3] border-[#F5E9D6] p-[30px] rounded-[12px]"
+          required
+          placeholder="제목을 입력해주세요.(필수)"
+        />
+        <textarea
+          className="bodrder-[3px] border-solid placeholder:text-[#B3B3B3] border-[#F5E9D6] p-[30px] rounded-[12px]"
+          required
+          placeholder="실패담의 내용을 입력해주세요.(필수)"
+        />
       </section>
-
     </div>
-  )
-}
+  );
+};
