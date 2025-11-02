ecb29b7e07a306e45d7025d84585b30c7c3258ce
Junwoo <a90846251@gmail.com>
2025-11-02 18:22:35 +0900

    글작성 페이지 작업시작. 부모 div 생성

---
+++ src/pages/postpage/PostWrite.tsx
@@ -0,0 +1,11 @@
+export const PostWrite = () => {
+  return (
+    <div className="px-[82px] pt-[60px] rounded-[30px] bg-[#FDFDFD] flex flex-col justify-center">
+      {/* 첫번째 섹션 글작성 ~ 글내용입력 */}
+      <section className="flex flex-col gap-10">
+
+      </section>
+
+    </div>
+  )
+}
