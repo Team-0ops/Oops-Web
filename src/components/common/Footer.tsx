003cde62080735d89ef0d836b7b3ce5240b3dc23
jangyeeunee <vkfkdto2053@gmail.com>
2026-01-19 19:13:47 +0900

    style: Fotter 스타일 변경사항 적용

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,19 +1,14 @@
-import { useIsMobile } from "../../hooks/common/useIsMobile";
-
 const Footer = () => {
-  //현재 모바일인지 아닌자 판별하는 훅 : boolean 반환
-  const isMobile = useIsMobile();
-
   return (
-    <footer className="flex w-full h-[8.125rem] border-t border-[#999]">
-      <div className="caption3 flex w-full pt-[1.87rem] pl-[5.12rem] sm:pl-[1.44rem] xl:pl-[5.12rem] gap-[2.56rem] text-[#CCC] ">
-        <div className="flex flex-col gap-[0.38rem]">
+    <footer className="w-full h-56.25 py-9.25 px-26.75">
+      <div className="caption2 flex w-full gap-24.75 text-[#6F6F6F] pt-10 border-t-2 border-[#B2B2B2]">
+        <div className="flex flex-col gap-7.5">
           <p className="hover:underline cursor-pointer">서비스 소개</p>
           <p className="hover:underline cursor-pointer">광고문의</p>
           <p className="hover:underline cursor-pointer">멤버 소개</p>
         </div>
 
-        <div className="flex flex-col gap-[0.38rem]">
+        <div className="flex flex-col gap-7.5">
           <p className="hover:underline cursor-pointer">이용약관</p>
           <p className="hover:underline cursor-pointer">개인정보 처리방침</p>
           <p className="hover:underline cursor-pointer">신고가이드</p>
