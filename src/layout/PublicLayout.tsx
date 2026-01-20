e6bcdd2d3a2ca30fb12de1dc6d200bf1c935fcc7
jangyeeunee <vkfkdto2053@gmail.com>
2026-01-20 19:19:24 +0900

    style: main 페이지 레이아웃 수정

--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -5,7 +5,7 @@ import Navbar from "../components/common/Navbar";
 const PublicLayout = () => {
   return (
     <div className="min-h-screen w-full bg-[#FFF]">
-        <div className="mx-auto max-w-[1600px] flex min-h-screen flex-col px-27">
+        <div className="mx-auto max-w-[1600px] flex min-h-screen flex-col px-27 gap-10">
             <Navbar />
             <Outlet />
             <Footer />
