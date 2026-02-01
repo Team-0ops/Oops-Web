f3944b1dbe04929efef013a1d21c42cd7e084274
jangyeeunee <vkfkdto2053@gmail.com>
2026-02-02 00:37:07 +0900

    fix: build error

--- src/components/SearchPage/ResultList.tsx
+++ src/components/SearchPage/ResultList.tsx
@@ -1,13 +1,11 @@
-import { PostCard } from "../common/PostCard";
-
 export const ResultList = () => {
   return (
     <>
       <div className="grid grid-cols-1 xl:grid-cols-2 xl:gap-[1.5rem] gap-[1.25rem]">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        {/*<PostCard />*/}
+        {/*<PostCard />*/}
+        {/*<PostCard />*/}
+        {/*<PostCard />*/}
       </div>
     </>
   );
