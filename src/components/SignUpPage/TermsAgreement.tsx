e598abe904ff96bde9ae750095df3d4b84e8e9d1
Junwoo <a90846251@gmail.com>
2026-02-13 18:07:51 +0900

    vercel 에러 해결

--- src/components/SignUpPage/TermsAgreement.tsx
+++ src/components/SignUpPage/TermsAgreement.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import Checkbox from "../common/Checkbox";
 import NextArrow from "../../assets/icons/NextArrow.svg?react";
 import { useLocation, useNavigate } from "react-router-dom";
