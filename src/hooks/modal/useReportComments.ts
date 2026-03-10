5ac00c55152eabdf1d0cbff52050b1414dfbb493
Junwoo <a90846251@gmail.com>
2026-03-10 10:57:30 +0900

    0310 로그인, 신고 훅 수정

--- src/hooks/modal/useReportComments.ts
+++ src/hooks/modal/useReportComments.ts
@@ -1,13 +1,29 @@
 import { useMutation } from "@tanstack/react-query";
 import { postReportComment } from "../../apis/Comment/postReportComment";
-
-type Variables = {
-    commentId : number;
-    content: string;
-}
+import axios from "axios";
 
 export const useReportComments = () => {
-    return useMutation({
-        mutationFn:({commentId, content}:Variables) => postReportComment({commentId, content})
-    });
+  return useMutation({
+    mutationFn: async (params: { commentId: number; content: string }) => {
+      try {
+        return await postReportComment(params);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          const status = error.response?.status;
+          const message = error.response?.data?.message;
+
+          if (status === 400) {
+            if (message === "자기 자신의 댓글/게시글은 신고할 수 없습니다.") {
+              throw new Error(message);
+            }
+
+            if (message === "이미 신고한 댓글/게시글입니다.") {
+              throw new Error(message);
+            }
+          }
+        }
+        throw new Error("신고 처리 중 오류가 발생했습니다.");
+      }
+    },
+  });
 };
--- src/hooks/modal/useReportPost.ts
+++ src/hooks/modal/useReportPost.ts
@@ -1,13 +1,30 @@
 import { useMutation } from "@tanstack/react-query";
-import { reportPost } from "../../apis/Post/reportPost"; 
-
-type Variables = {
-  postId: number;
-  content: string;
-};
+import axios from "axios";
+import { reportPost } from "../../apis/Post/reportPost";
 
 export const useReportPost = () => {
-    return useMutation({
-    mutationFn: ({ postId, content }: Variables) => reportPost({ postId, content }),
+  return useMutation({
+    mutationFn: async (params: { postId: number; content: string }) => {
+      try {
+        return await reportPost(params);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          const status = error.response?.status;
+          const message = error.response?.data?.message;
+
+          if (status === 400) {
+            if (message === "자기 자신의 댓글/게시글은 신고할 수 없습니다.") {
+              throw new Error(message);
+            }
+
+            if (message === "이미 신고한 댓글/게시글입니다.") {
+              throw new Error(message);
+            }
+          }
+        }
+
+        throw new Error("신고 처리 중 오류가 발생했습니다.");
+      }
+    },
   });
 };
